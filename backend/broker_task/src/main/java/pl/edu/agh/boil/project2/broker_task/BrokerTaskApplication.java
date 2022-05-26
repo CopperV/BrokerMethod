547b38bc48ff3302c71511a27d93db58755450c2
Błażej Rosiak <vark123@interia.pl>
2022-05-26 16:45:12 +0200

    Backend

---
+++ backend/broker_task/src/main/java/pl/edu/agh/boil/project2/broker_task/BrokerTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.boil.project2.broker_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerTaskApplication.class, args);
+	}
+
+}
